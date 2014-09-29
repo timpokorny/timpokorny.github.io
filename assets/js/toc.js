65dfa857ad77b486405fc57af8b21ebde45aacec
Tim Pokorny <tim.pokorny@calytrix.com>
2014-09-29 21:21:39 +0800

    Added in the basic template for a FAQ and made the TOC & syntax highlighting options available more broadly.

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -10,7 +10,7 @@
     },
     settings = $.extend(defaults, options);
 
-    var headers = $('.post-content h1, .post-content h2').filter(function() {
+    var headers = $('.post-content h1, .post-content h2, .faq h1, .faq h2').filter(function() {
       // get all headers with an ID
       if (!this.id) {
         this.id = $(this).attr( "id", $(this).prev().attr( "name" ) );
